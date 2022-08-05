eaf31f2f1817be5220328d8f16a3b56a697885d5
Eloisa Cardona <eloisacardona43@gmail.com>
2022-08-05 16:27:40 -0500

    usersModel editando

--- src/api/models/usersModel.js
+++ src/api/models/usersModel.js
@@ -3,7 +3,7 @@ const { validateEmail, validatePassword } = require("../../utils/regex");
 
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  firtsName: {
+  firstName: {
     type: String,
     required: true,
   },
